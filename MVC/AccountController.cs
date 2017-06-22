6070a9d4aca5d478d0b68ca402207e221abeb6d1
Billy Song <billy.song12@gmail.com>
2017-06-22 15:49:49 -0700

    Add files via upload

---
+++ MVC/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Sabio.Web.Models;
+using Sabio.Web.Controllers.BaseControllers;
+
+// ********** FYI: System Generated File
+
+
+namespace Sabio.Web.Controllers
+{   
+    public class AccountController : SiteController
+    {
+        public ActionResult AccountWizard()
+        {
+            return View();
+        }
+        [Route("~/Account")]
+        public ActionResult AccountWizardNg()
+        {
+            return View();
+        }
+    }
+}
